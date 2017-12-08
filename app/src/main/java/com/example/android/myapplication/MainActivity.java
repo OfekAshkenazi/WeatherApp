b237630aaae451a33c16bb98ef070295562cc0a8
OfekAshkenazi <regevofek@live.com>
2017-12-08 19:15:59 +0200

    ...

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import DarkSkyService.SkyDarkService;
 import DarkSkyService.WeatherInfo;
@@ -15,28 +16,36 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String BASE_URL = "https://api.darksky.net/forecast/";
     private static final String API_KEY = "371eb1171240e12f8b581216ca82c7c4/" ;
+    private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Retrofit retrofit= new Retrofit.Builder()
+        retrofit= new Retrofit.Builder()
                 .baseUrl(BASE_URL+API_KEY)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        WeatherInfo weatherInfo=getWeatherInfo();
+    }
+
+    public WeatherInfo getWeatherInfo() {
+        final WeatherInfo[] weatherInfo = new WeatherInfo[1];
         SkyDarkService client=retrofit.create(SkyDarkService.class);
 
         Call<WeatherInfo> call=client.getWeatherByLocation("32.7911269","35.0349122");
         call.enqueue(new Callback<WeatherInfo>() {
             @Override
             public void onResponse(Call<WeatherInfo> call, Response<WeatherInfo> response) {
-
+                weatherInfo[0] =response.body();
             }
 
             @Override
             public void onFailure(Call<WeatherInfo> call, Throwable t) {
-
+                Log.v("Call Failed, URL:   ",call.request().url().toString());
             }
         });
+
+        return weatherInfo[0];
     }
 }
